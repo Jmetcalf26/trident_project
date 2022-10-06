23ad44e658efcd76da19703d8d2da8517b2ee190
jmetcalf26 <jackmetcalf26@gmail.com>
2022-10-06 09:38:16 -0400

    worked on the integer stuff as well as the macro information stuff

--- output_file.py
+++ output_file.py
@@ -1,17 +1,17 @@
 from helper_classes import *
-balls = 10
-balls2 = 'HEY'
+from pheaders.stdio import *
+
+
+def booga():
+    return 10
 
 
 def main():
     a = [0]
     b = [1]
     c = [a[0] + b[0]]
-    d = [3]
-    e = [1.5]
-    f = [1.5]
-    g = [1.5]
-    printf(['%d%d\n'], [a[0]], [c[0]])
+    h = ['%d%d\n']
+    printf([h[0]], [a[0]], [c[0]])
 
 
 if __name__ == '__main__':
--- simple.c
+++ simple.c
@@ -1,12 +1,9 @@
-#define end_of_metadata_macros 10
-#include <limits.h>
+#include <stdio.h>
+// what the heck!
 int main(){
 	int a = INT_MAX;
 	char b = 1;
 	int c = a + b;
-	unsigned d = 3;
-	double e = 1.5;
-	long f = 1.5;
-	float g = 1.5;
-	printf("%d%d\n", a, c);
+	char * h = "%d%d\n";
+	printf(h, a, c);
 }
